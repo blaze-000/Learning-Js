3e7d6ea6bb76679ce5307469114cfaa60b175e42
blaze-000 <asmitsah36@gmail.com>
2025-06-25 22:36:37 +0545

    #3

---
+++ conversionOperation.js
@@ -0,0 +1,14 @@
+let value = undefined
+
+console.log(typeof value);
+
+let convert = Number(value)
+
+console.log(convert);
+
+//"33" => 33
+//"33aa" => Nan = Not a Number
+// true => 1 || false => 0
+ let str = 2
+ console.log(str);
+ 
