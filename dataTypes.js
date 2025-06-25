c5adc45306c2bb568950bc5d31db8e466a9eb0d0
blaze-000 <asmitsah36@gmail.com>
2025-06-25 22:26:14 +0545

    02

---
+++ dataTypes.js
@@ -0,0 +1,17 @@
+let name = "Asmit"
+let age = 25
+let isStudent = true;
+let hobbies = ["reading", "gaming", "coding"];
+
+
+// number - 2 to the power of 53
+// string
+//boolean => True or false
+//null - It is object => standalone value
+//undefined - It is undefined 
+//symbol - unique value
+
+
+//object - collection of key value pairs
+
+console.log(typeof name);
